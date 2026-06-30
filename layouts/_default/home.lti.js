2f3da23360da73834979a373deb03754904627f5
Russell Feldhausen <russfeldh@gmail.com>
2026-06-30 11:17:38 -0500

    Cleanup JSON

--- layouts/_default/home.lti.js
+++ layouts/_default/home.lti.js
@@ -1,3 +1,3 @@
 {{- /* [x] russfeld */}}
 {{- $tree := partial "_relearn/ltiPageTree.gotmpl" .Site.Home }}
-var relearn_lti_index = {{ $tree | jsonify (dict "indent" "  ") }}
+{{ $tree | jsonify (dict "indent" "  ") }}
